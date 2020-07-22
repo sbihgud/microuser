ad823930671952d6fbb53c0c7956a5ed98780bdf
xiaoping <sbihgu618@gmail.com>
2020-07-22 14:29:11 +0800

    test

--- src/micro/interfaces/UserServiceInterFace.php
+++ src/micro/interfaces/UserServiceInterFace.php
@@ -4,5 +4,19 @@ namespace anonymity\micro\interfaces;
 
 interface UserServiceInterFace
 {
-    public function add(int $a, int $b);
+    /**
+     * 加
+     * @param int $a
+     * @param int $b
+     * @return mixed
+     */
+    public function bcAdds(int $a, int $b);
+
+    /**
+     * 减
+     * @param int $a
+     * @param int $b
+     * @return mixed
+     */
+    public function bcSubs(int $a, int $b);
 }
